fa1daeeb1111fbdadd6b436439f9822e05afe48f
Feiyang <plane1113@gmail.com>
2016-03-20 18:40:19 -0400

    url change fpr production

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,8 +50,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
         app.newExcerciseBpm = 0;
     };
 
-    // app.dataUrl = "api";
-    app.dataUrl = "http://localhost:3030/api";
+    app.dataUrl = "api";
+    //app.dataUrl = "http://localhost:3030/api";
     app.excerciseUpdatedUrl = app.dataUrl + "/update";
     app.excerciseCompletedUrl = app.dataUrl + "/complete";
 
