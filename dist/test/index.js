3ace959c689f2c2645fdf3b993f270b9489b0355
Feiyang <plane1113@gmail.com>
2017-02-08 21:15:53 -0500

    purge workspace

--- dist/test/index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-WCT.loadSuites(['my-greeting-basic.html', 'my-greeting-basic.html?dom=shadow', 'my-list-basic.html', 'my-list-basic.html?dom=shadow', 'mytest-element-basic.html', 'mytest-element-basic.html?dom=shadow', 'drum-excercise-basic.html', 'drum-excercise-basic.html?dom=shadow']);
