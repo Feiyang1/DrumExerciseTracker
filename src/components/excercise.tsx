b9f2554e1eb461e3b41d15e54726745565b2bd27
Feiyang <plane1113@gmail.com>
2017-02-12 20:56:41 -0500

    react version init

---
+++ src/components/excercise.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+interface IExcerciseProps { };
+interface IExcerciseState { };
+
+class Excercise extends React.Component<IExcerciseProps, IExcerciseState> {
+    public render(){
+        return (
+            <div></div>
+        );
+    }
+}
