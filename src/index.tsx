2fc4ba5585e0b773c9d83428efd0ac5fa2aa1452
Feiyang <plane1113@gmail.com>
2017-02-14 21:37:07 -0500

    add appbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as injectTapEventPlugin from "react-tap-event-plugin";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import IconButton from "material-ui/IconButton";
+import NavigationClose from "material-ui/svg-icons/navigation/close";
 
 import data from "./data";
 import Excercise from "./components/excercise";
 
+injectTapEventPlugin();
+
 ReactDOM.render(
     <MuiThemeProvider>
-        <Excercise model={data[0]}/>
+        <div>
+        <AppBar title="Drum Pad"
+        iconElementLeft={<div></div>}
+        />
+        {
+            data.map((excercise)=>{
+                return <Excercise model={excercise}/>
+            })
+        }
+        </div>
     </MuiThemeProvider>,
     document.getElementById("example")
 );
