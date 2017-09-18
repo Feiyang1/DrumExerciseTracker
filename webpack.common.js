6f1960e1a9eb0139585da8fb51d39f361110f93b
Feiyang Chen <plane1113@gmail.com>
2017-09-17 21:09:31 -0400

    update build process

--- webpack.common.js
+++ webpack.common.js
@@ -51,6 +51,10 @@ const frontend = {
         ]
     },
 
+    plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ],
+
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
