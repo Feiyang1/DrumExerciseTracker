769b51cceb3921575047260675ab002ff3de7024
Feiyang Chen <plane1113@gmail.com>
2018-07-22 11:29:21 -0400

    introduce authentication

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,14 +4,63 @@ import AppBar from "../containers/appBarContainer";
 import VisibleExcerciseList from "../containers/visibleExcerciseList";
 import Header from "./header";
 import Dialog from "../containers/dialogContainer";
+import * as firebase from "firebase";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-const App: React.StatelessComponent<any> = () => {
-    return <div>
-        <AppBar />
-        <Header />
-        <VisibleExcerciseList />
-        <Dialog />
-    </div>
+
+const authConfiguration = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        // Avoid redirects after sign-in.
+        signInSuccessWithAuthResult: () => false
+    }
+};
+
+interface AppState {
+    isSignedIn: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+    unregisterAuthObserver: any;
+    constructor() {
+        super();
+        // The component's Local state.
+        this.state = {
+            isSignedIn: false // Local signed-in state.
+        };
+    }
+
+    componentDidMount(): void {
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+            (user) => this.setState({isSignedIn: !!user})
+        );
+      }
+
+    render(): JSX.Element {
+        return <div>
+            {
+                this.state.isSignedIn ?
+                <div>
+                    <div onClick={this.signOut}>sign out</div>
+                    <AppBar />
+                    <Header />
+                    <VisibleExcerciseList />
+                    <Dialog />
+                </div> :
+                <StyledFirebaseAuth uiConfig={authConfiguration} firebaseAuth={firebase.auth()} />
+            }
+        </div>
+    }
+
+    signOut(): void {
+        firebase.auth().signOut();
+    }
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,30 @@ import thunkMiddleware from "redux-thunk";
 import logger from "redux-logger";
 import { AppContainer } from "react-hot-loader";
 
- import App from "./components/app";
- import { fetchExcercises } from "./actions";
- import excerciseApp from "./reducers/excercises";
- import data from "./data";
- import * as Models from "./models";
+import App from "./components/app";
+import { fetchExcercises } from "./actions";
+import excerciseApp from "./reducers/excercises";
+import data from "./data";
+import * as Models from "./models";
+import * as firebase from 'firebase';
 
-let store = createStore<Models.DrumExcerciseStore>(excerciseApp, { 
-    excercises: data, 
-    visibilityFilter: "today", 
-    dialog: { component: null, props: null, show: false},
-    uiState: {showAddExcericeModal: false} }, applyMiddleware(logger, thunkMiddleware));
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyB-39dPhBmprjGMhREEaKubQ9RSR0yF2SQ",
+    authDomain: "meealwaysawesome.firebaseapp.com",
+    databaseURL: "https://meealwaysawesome.firebaseio.com",
+    projectId: "meealwaysawesome",
+    storageBucket: "meealwaysawesome.appspot.com",
+    messagingSenderId: "190884510771"
+};
+firebase.initializeApp(config);
+
+let store = createStore<Models.DrumExcerciseStore>(excerciseApp, {
+    excercises: data,
+    visibilityFilter: "today",
+    dialog: { component: null, props: null, show: false },
+    uiState: { showAddExcericeModal: false }
+}, applyMiddleware(logger, thunkMiddleware));
 store.dispatch(fetchExcercises()).then(() => {
     console.log("fetch success");
 });
